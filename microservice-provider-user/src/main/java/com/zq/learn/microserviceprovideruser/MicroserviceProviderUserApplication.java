ca738686fd0b1bf16bf2e97a90684f2a9a45cd8b
qun.zheng <zhengqun@youedata.com>
2018-02-26 17:31:54 +0800

    初始导入

---
+++ microservice-provider-user/src/main/java/com/zq/learn/microserviceprovideruser/MicroserviceProviderUserApplication.java
@@ -0,0 +1,13 @@
+package com.zq.learn.microserviceprovideruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class MicroserviceProviderUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceProviderUserApplication.class, args);
+	}
+}
